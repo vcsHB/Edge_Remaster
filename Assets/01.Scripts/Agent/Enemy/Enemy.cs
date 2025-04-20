b878ed86cb64384f6b1a9269faa3446403b74658
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-20 23:16:19 +0900

    feat: Enemy State Machine Script, Sprite

---
+++ Assets/01.Scripts/Agent/Enemy/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Agents.Enemies
+{
+    public class Enemy : Agent
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            
+        }
+    }
+
+}
---
+++ Assets/01.Scripts/Agent/Enemy/FSM/EnemyStateMachine.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+namespace Agents.Enemies.FSM
+{
+
+    public class EnemyStateMachine
+    {
+
+    }
+}
