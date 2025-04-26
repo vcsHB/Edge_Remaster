ad9709c05d15c40f0fa9eebfcf0bc84f87df39f8
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-04-26 23:48:52 +0900

    feat: Reward SO

--- Assets/01.Scripts/Agent/Enemy/RewardSystem/RewardDropper.cs
+++ Assets/01.Scripts/Agent/Enemy/RewardSystem/RewardDropper.cs
@@ -1,4 +1,4 @@
-using UnityEditor;
+using Combat;
 using UnityEngine;
 namespace RewardSystem
 {
@@ -6,11 +6,17 @@ namespace RewardSystem
     public class RewardDropper : MonoBehaviour
     {
         [SerializeField] private RewardSO _rewardData;
- 
+        private Health _healthCompo;
+
+        private void Awake()
+        {
+            _healthCompo = GetComponent<Health>();
+            _healthCompo.OnDieEvent.AddListener(DropReward);
+        }
         public void DropReward()
         {
-            
-            
+            PoolManager.Instance.Pop(ObjectPooling.PoolingType.SoundPlayer);
+
         }
     }
 }
