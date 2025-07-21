2e17aa168b0741443c06d4bd5eeadf5669c4530a
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-07-21 23:59:26 +0900

    feat: Unlock VFX
    replace soundPlayer Pooling Data.

--- Assets/01.Scripts/Utils/PoolManager/Core/ObjectPool/PoolingType.cs
+++ Assets/01.Scripts/Utils/PoolManager/Core/ObjectPool/PoolingType.cs
@@ -2,6 +2,6 @@ namespace ObjectPooling
 {
     public enum PoolingType
     {
-        SoundPlayer,PlayerProjectile,ProjectileDestroyVFX,EnemyGenerateVFX,EnemyDestroyVFX,CrystalObject,GhostShadowVFX,PlayerBombProjectile,PlayerBombProjectileDestroyVFX,CannonBulletDestroyVFX,CannonBullet,StructureBuildVFX,StructureDestroyVFX,EnemyProjectile_1,EnemyProjectile_2,
+        UnlockVFX,PlayerProjectile,ProjectileDestroyVFX,EnemyGenerateVFX,EnemyDestroyVFX,CrystalObject,GhostShadowVFX,PlayerBombProjectile,PlayerBombProjectileDestroyVFX,CannonBulletDestroyVFX,CannonBullet,StructureBuildVFX,StructureDestroyVFX,EnemyProjectile_1,EnemyProjectile_2,
     }
 }
