d429273b62da3dcd982ed7c78f21d36a9135b79c
vcsHB <99899703+vcsHB@users.noreply.github.com>
2025-07-28 23:45:25 +0900

    fix: rename Function

--- Assets/01.Scripts/StageSystem/TutorialStep/UpgradeStep.cs
+++ Assets/01.Scripts/StageSystem/TutorialStep/UpgradeStep.cs
@@ -23,12 +23,12 @@ namespace StageSystem.TutorialManage
                 Debug.LogError("Build Event Channel is Not binded");
                 return;
             }
-            _buildEventChannel.AddListener<UpgradeData>(HandleBuild);
+            _buildEventChannel.AddListener<UpgradeData>(HandleUpgrade);
             // 스텝에 들어온 시점에. 해당 위치에 이미 설치된 것에 대한 예외 처리 필요
             // todo. 
         }
 
-        private void HandleBuild(GameEvent data)
+        private void HandleUpgrade(GameEvent data)
         {
             if (data is UpgradeData UpgradeData)
             {
@@ -46,7 +46,7 @@ namespace StageSystem.TutorialManage
         public override void Exit()
         {
             base.Exit();
-            _buildEventChannel.RemoveListener<UpgradeData>(HandleBuild);
+            _buildEventChannel.RemoveListener<UpgradeData>(HandleUpgrade);
 
         }
     }
